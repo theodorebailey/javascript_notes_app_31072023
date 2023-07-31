38a3d555bacfb53919075cde2c83f079dd08c0cd
Theodore Bailey <tawbailey@hotmail.com>
2023-07-31 17:17:49 +0100

    local storage not functioning

--- js.js
+++ js.js
@@ -6,7 +6,15 @@ const create_btn = document.querySelector(".btn");
 // select all notes so we can iterate through
 const notes = document.querySelectorAll(".input-box");
 
+function show_notes (){
+    notes_container.innerHTML = localStorage.getItem("notes");
+}
 
+show_notes();
+
+function update_storage() {
+    localStorage.setItem("notes", notes_container.innerHTML);
+}
 // add event listener to create button which is an onclick even which triggers a function which creates a paragraph element and saves to varaible name input_box then create img element, give input_box a class name and attribute, then give img element a src of delete icon and append child of notes_container with img nested in input_box
 create_btn.addEventListener("click", ()=> {
     let input_box = document.createElement("p");
@@ -19,8 +27,28 @@ create_btn.addEventListener("click", ()=> {
 
 })
 
+// target notes_container element, add event listener of function with event parameter which we use to evaluate if its the targeted element is an image element we'll remove the parent element i.e. using the delete icon and execute updateStorage(), else if the event target tagname is equal to a paragraph element we will select all elements reassign to variable notes which has already been declared and save the documentation to localStorage using a forEach iterative function with paramter nt (notes) on keyup which is an event attribute which executes when a key is lifted up (released) in any of the nt elements
 notes_container.addEventListener("click", (e)=> {
     if(e.target.tagName === "IMG"){
         e.target.parentElement.remove();
+        update_storage();
+    } else if (e.target.tagName === "p") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                update_storage();
+            }
+        })
+
+    }
+})
+
+// add local storage
+
+// Allows enter key to execute a linebreak
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
 })
